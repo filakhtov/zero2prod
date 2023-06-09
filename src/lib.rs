5391f06e87a5517b6a397f89ff2c985bb6f03604
Garry Filakhtov <filakhtov@gmail.com>
2023-06-10 00:29:44 +1000

    no-op implementation of the /subscribers endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
 async fn health_check() -> impl Responder {
     HttpResponse::NoContent()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    Ok(
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .listen(listener)?
-            .run(),
-    )
+    Ok(HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,29 +1,96 @@
+use reqwest::{Client, StatusCode};
 use std::net::TcpListener;
 
-use reqwest::StatusCode;
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to the random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = z2p::run(listener).expect("Failed to run the app");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_responds_with_204_and_no_content() {
-    let port = spawn_app().await;
+    let address = spawn_app();
 
-    let client = reqwest::Client::new();
+    let client = Client::new();
 
     let response = client
-        .get(format!("http://127.0.0.1:{}/health_check", port))
+        .get(format!("{}/health_check", address))
         .send()
         .await
-        .expect("Failed to send a request to our app");
+        .expect("Failed to send a request to the app");
 
     assert!(response.status().is_success());
     assert_eq!(StatusCode::NO_CONTENT, response.status());
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> u16 {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to the random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = z2p::run(listener).expect("Failed to bind the address");
-    let _ = tokio::spawn(server);
+#[tokio::test]
+async fn subscribe_responds_with_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = Client::new();
 
-    port
+    let body = "name=john%20doe&email=john.doe%40example.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send a request to the app");
+
+    assert_eq!(StatusCode::OK, response.status())
+}
+
+#[tokio::test]
+async fn subscribe_responds_with_400_when_email_is_missing() {
+    let address = spawn_app();
+    let client = Client::new();
+
+    let body_without_email = "name=Alice%20Smith";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body_without_email)
+        .send()
+        .await
+        .expect("Failed to send a request to the app");
+
+    assert_eq!(StatusCode::BAD_REQUEST, response.status());
+}
+
+#[tokio::test]
+async fn subscribe_responds_with_400_when_name_is_missing() {
+    let address = spawn_app();
+    let client = Client::new();
+
+    let body_without_name = "email=alice.smith%40example.com";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body_without_name)
+        .send()
+        .await
+        .expect("Failed to send a request to the app");
+
+    assert_eq!(StatusCode::BAD_REQUEST, response.status());
+}
+
+#[tokio::test]
+async fn subscribe_responds_with_400_when_email_and_name_are_missing() {
+    let address = spawn_app();
+    let client = Client::new();
+
+    let empty_body = "";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(empty_body)
+        .send()
+        .await
+        .expect("Failed to send a request to the app");
+
+    assert_eq!(StatusCode::BAD_REQUEST, response.status());
 }
