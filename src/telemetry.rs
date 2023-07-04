5320e908cbff6b45f4d5a9b76b3f29780112448d
Garry Filakhtov <garry.filakhtov@bigcommerce.com>
2023-07-04 20:43:21 +1000

    extract spawn blocking + tracing into a helper function

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,3 +1,4 @@
+use tokio::task::JoinHandle;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -20,4 +21,13 @@ pub fn get_subscriber<Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static>(
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to initialize tracing logger");
     set_global_default(subscriber).expect("Failed to attach logging subscribers");
+}
+
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    let current_span = tracing::Span::current();
+    tokio::task::spawn_blocking(move || current_span.in_scope(f))
 }
